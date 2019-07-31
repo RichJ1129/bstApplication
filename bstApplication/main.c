a9d10cd4fba986c3b2928cbf09ae2cb99df364c8
Richard Joseph <richjoseph1129@gmail.com>
2019-07-30 23:21:33 -0400

    Initial Commit

---
+++ bstApplication/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  bstApplication
+//
+//  Created by Richard Joseph on 7/30/19.
+//  Copyright © 2019 Richard Joseph. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
